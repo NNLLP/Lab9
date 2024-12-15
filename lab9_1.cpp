d86c5b0d7311f951a04e3d5a931dc11fd4bd9bb7
NNLLP <98796613+NNLLP@users.noreply.github.com>
2024-12-15 11:24:09 +0700

    lab9_1.cpp

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,13 +3,18 @@
 using namespace std;
 
 int main(){	
+	double x, y, z;
 	cout << "Enter initial loan: ";
+	cin >> x;
 	cout << "Enter interest rate per year (%): ";
+	cin >> y;
 	cout << "Enter amount you can pay per year: ";
+	cin >> z;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+
+	double a = x;
+	int year = 1;
+
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -18,16 +23,23 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	while(a > 0.01){
+		double interest = a * (y/100);
+		double total = a + interest;
+		double pay = (total < z) ? total : z;
+		double newp = total-pay;
+
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << year; 
+		cout << setw(13) << left << a;
+		cout << setw(13) << left << interest;
+		cout << setw(13) << left << total;
+		cout << setw(13) << left << pay;
+		cout << setw(13) << left << newp;
+		cout << "\n";
+
+		a = newp;
+		year++;
+	}
 	return 0;
 }
